4b361c95bbf7f7294f8d503f41b1c32a0141722e
MuhSaktiR <seken876@gmail.com>
2024-11-21 19:48:32 +0700

    Membuat fungsi CardSection

--- app/src/main/java/com/example/arsitektumvvm/ui/view/DetailMahasiswaView.kt
+++ app/src/main/java/com/example/arsitektumvvm/ui/view/DetailMahasiswaView.kt
@@ -4,6 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import com.example.arsitektumvvm.model.DataMahasiswa
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun DetailMahasiswaView(
@@ -22,7 +29,27 @@ fun DetailMahasiswaView(
 
     Column(){
         listDataMhs.forEach{ items ->
+            CardSection(
+                judulParam = items.first,
+                isiParam = items.second
+            )
+        }
+    }
+}
 
+@Composable
+fun CardSection(judulParam: String, isiParam: String) {
+    Column {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = judulParam, modifier = Modifier.weight(0.8f))
+            Text(text = ":", modifier = Modifier.weight(0.2f))
+            Text(
+                text = "$isiParam",
+                modifier = Modifier.weight(2f)
+            )
         }
     }
 }
